1a1f8a7d0b9492975d2340f329c008c4847f9e04
Nik314 <niklasvandetten@arcor.de>
2026-04-27 18:11:27 +0200

    additional models

--- example_journal.py
+++ example_journal.py
@@ -7,6 +7,21 @@ from src_journal import extended_df2_miner_apply
 
 
 
+#case study for the showcase application --> order management model with synchronizations
+blocked = ["subset_sync", "implication"]
+eocpt = extended_df2_miner_apply("data/01_ocel_standard_order_management.json",
+                        0.90,1.00,blocked)
+print(str(eocpt))
+exit()
+
+
+#case study for the showcase application --> order management model with implications
+blocked = ["subset_sync", "strict_sync"]
+eocpt = extended_df2_miner_apply("data/01_ocel_standard_order_management.json",
+                        0.90,1.00,blocked)
+print(str(eocpt))
+
+
 
 
 
@@ -24,15 +39,8 @@ eocpt = extended_df2_miner_apply("data/10_ocel_legacy_recruiting.jsonocel",
 print(str(eocpt))
 
 
-#case study for the showcase application --> order management model with synchronizations
-blocked = ["subset_sync", "implication"]
-eocpt = extended_df2_miner_apply("data/01_ocel_standard_order_management.json",
-                        0.90,1.00,blocked)
-print(str(eocpt))
-
-
-#case study for the showcase application --> order management model with implications
+#case study for the showcase application --> recruitment model with implications
 blocked = ["subset_sync", "strict_sync"]
-eocpt = extended_df2_miner_apply("data/01_ocel_standard_order_management.json",
-                        0.90,1.00,blocked)
+eocpt = extended_df2_miner_apply("data/10_ocel_legacy_recruiting.jsonocel",
+                        0.90,0.90,blocked)
 print(str(eocpt))

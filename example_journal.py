80198f731b83446f0f24e9ddbc1bbd809fd95b38
Nik314 <niklasvandetten@arcor.de>
2026-04-27 16:06:40 +0200

    added example application extension

---
+++ example_journal.py
@@ -0,0 +1,38 @@
+
+
+from src_journal import extended_df2_miner_apply
+
+
+
+
+
+
+
+
+
+#case study for the showcase application --> recruitment model with synchronizations
+blocked = ["subset_sync", "implication"]
+eocpt = extended_df2_miner_apply("data/10_ocel_legacy_recruiting.jsonocel",
+                        0.90,0.7,blocked)
+print(str(eocpt))
+
+
+#case study for the showcase application --> recruitment model with implications
+blocked = ["subset_sync", "strict_sync"]
+eocpt = extended_df2_miner_apply("data/10_ocel_legacy_recruiting.jsonocel",
+                        0.90,1.00,blocked)
+print(str(eocpt))
+
+
+#case study for the showcase application --> order management model with synchronizations
+blocked = ["subset_sync", "implication"]
+eocpt = extended_df2_miner_apply("data/01_ocel_standard_order_management.json",
+                        0.90,1.00,blocked)
+print(str(eocpt))
+
+
+#case study for the showcase application --> order management model with implications
+blocked = ["subset_sync", "strict_sync"]
+eocpt = extended_df2_miner_apply("data/01_ocel_standard_order_management.json",
+                        0.90,1.00,blocked)
+print(str(eocpt))
--- plot_journal.py
+++ plot_journal.py
@@ -20,57 +20,5 @@ ax.set_xticks([0.5 + i for i in range(0, 12)])
 ax.set_xticklabels([str(i) + "                 " for i in range(1, 11)]+["",""])
 plt.savefig("runtime.png")
 #plt.show()
-exit()
 
 
-strict_rank = {
-    "strict":0,
-   "partition":1,
-   "overlap":2,
-  "order":3,
-    "batch":4,
-    "concurrent":5
-}
-
-
-
-def evaluate_relations_average_coverage(relation_string):
-    relation = eval(relation_string)
-    result = {key:[] for key in strict_rank.keys()}
-    for operator, activities in relation:
-        for key in result.keys():
-            if key in operator.lower():
-                result[key].append(len(activities))
-    return sum([(sum(result[key]) / len(result[key]) if result[key] else 0) for key in result.keys()]) / 6
-
-
-def evaluate_relations_per_kind(relation_string,key):
-    relation = eval(relation_string)
-    return len([operator for operator,activities in relation if key in operator.lower()])
-
-
-def evaluate_relation_count(relation_string):
-    relation = eval(relation_string)
-    result = {operator for operator,activities in relation}
-    return len(result)
-
-data["Average Activity Coverage"] = data["Relations"].apply(lambda entry:evaluate_relations_average_coverage(entry))
-data["Average Relation Count"] = data["Relations"].apply(lambda entry:evaluate_relation_count(entry))
-
-
-
-for key in strict_rank.keys():
-    print(key)
-    data[key] = data["Relations"].apply(lambda entry: evaluate_relations_per_kind(entry, key))
-    print(data.groupby("Parameter1")[key].mean())
-    print(data.groupby("Parameter2")[key].mean())
-
-print(data.groupby("Noise Parameter Sum")["Average Relation Count"].mean())
-print(data.groupby("Parameter1")["Average Relation Count"].mean())
-print(data.groupby("Parameter2")["Average Relation Count"].mean())
-
-
-blocked = ["subset_sync", "implication"]
-eocpt = extended_df2_miner_apply("data/10_ocel_legacy_recruiting.jsonocel",
-                        0.90,0.7,blocked)
-print(str(eocpt))
